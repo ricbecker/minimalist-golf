435d8464cf6542dc982bfdda68fdce98f0e8f42c
ricbecker <ric@ricbecker.com>
2017-04-15 12:36:45 +0000

    ok

--- breakbeam.py
+++ breakbeam.py
@@ -1,18 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-PIR_PIN=7
+GPIO.setmode(GPIO.BOARD)
+PIR_PIN=11
 GPIO.setup(PIR_PIN,GPIO.IN)
 
 try:
-    print "PIR Module Test (CTRL+C to exit)"
+    print ("PIR Module Test (CTRL+C to exit)")
     time.sleep(2)
-    print "Ready"
+    print ("Ready")
     while True:
         if GPIO.input(PIR_PIN):
-            print "Motion Detekalected beyotch"
-        time.sleep(1)
+            print ("Motion Detekalected beyotch")
+        time.sleep(.01)
 except KeyboardInterrupt:
-    print "Quit"
+    print ("Quit")
     GPIO.cleanup()
