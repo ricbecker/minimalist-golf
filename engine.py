903ba4ee3fe9b109eb7622ff6bce764fc8884546
Ric Becker <ricbecker@Rics-Macbook.local>
2017-04-15 08:37:54 -0400

    ok2

--- engine.py
+++ engine.py
@@ -1,27 +1,37 @@
 import loop
+import breakbeam
 
-playlist=["first.wav","second.wav","third.wav","fourth.wav"]
-track=[]
-
-
-for x in range(len(playlist)):
-    track.append('')
-    track[x]=loop.Looper(playlist[x])
-    track[x].start()
+def setup():
+    playlist=["first.wav","second.wav","third.wav","fourth.wav"]
+    track=[]
 
 
-        
+    for x in range(len(playlist)):
+        track.append('')
+        track[x]=loop.Looper(playlist[x])
+        track[x].start()
+
+    senslist=[11,12,35]
+    sensor=[]
+
+    for x in range(len(playlist)):
+        sensor.append('')
+        sensor[x]=breakbeam.Detect(sensor[x])
+
+
+
 def stopall():
     for x in range(len(track)):
         track[x].stop()
-        
+            
 def playall():
     for x in range(len(track)):
         track[x].play()
 
 
 if __name__ == '__main__':
-    playall()
+    setup()
+    
 
 
         
