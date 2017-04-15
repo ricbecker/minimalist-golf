d8efd14ccf2abfbfc7b99d86feb81d410a189bcb
Ric Becker <ricbecker@Rics-Macbook.local>
2017-04-15 08:44:23 -0400

    ok

--- breakbeam.py
+++ breakbeam.py
@@ -8,13 +8,13 @@ class Detect(threading.Thread):
         GPIO.setup(PIR_PIN,GPIO.IN)
 
     try:
-        print "PIR Module Test (CTRL+C to exit)"
+        print "(PIR Module Test (CTRL+C to exit))"
         time.sleep(2)
-        print "Ready"
+        print "(Ready)"
         while True:
             if GPIO.input(PIR_PIN):
-                print "Motion Detekalected beyotch"
+                print ("Motion Detekalected beyotch")
             time.sleep(1)
     except KeyboardInterrupt:
-        print "Quit"
+        print ("Quit")
         GPIO.cleanup()
